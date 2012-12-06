ea2d9698be02f587504d4ab0f53e6bffb4f8fdc8
liwenfeng20112012@gmail.com <liwenfeng20112012@gmail.com@88132001-a776-28e8-4add-e9d731fe8be2>
2012-12-06 03:26:51 +0000

    This is the full version date layer design include DTO, DAO and factory, other layer just need use factory create DAO, then get the DTO Object. I also upload a structure diagrams of the data layer your can have a check and reference.

---
+++ FacilityBookingSystem/src/daoimp/BaseConnection.java
@@ -0,0 +1,120 @@
+package data.daoimp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class BaseConnection {
+	protected Connection conn;
+	public BaseConnection(){
+		ConnecterBean cb = null;
+		try {
+			File configFile = new File("config.xml");
+			InputSource ins = new InputSource(new FileInputStream(configFile));
+			cb = SAXParseConnecter.xmlOpera(ins);
+			Class.forName(cb.getDriver());
+			conn = DriverManager.getConnection(cb.getDatabase(),cb.getUsername(),cb.getPassword());
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		conn.close();
+		super.finalize();
+	}
+}
+
+class SAXParseConnecter {
+	public static ConnecterBean xmlOpera(InputSource instream){
+		SAXParserFactory saxparseFac = SAXParserFactory.newInstance();
+		try {
+			SAXParser saxPar =saxparseFac.newSAXParser();
+			XMLContentHandlerForBean xmlHandler = new XMLContentHandlerForBean();
+			saxPar.parse(instream,xmlHandler);
+			return xmlHandler.getConnecterBean();
+		} 
+		catch (Exception e) {
+			return null;
+		}
+	}
+}
+
+class XMLContentHandlerForBean extends DefaultHandler{
+		private ConnecterBean bean;
+		private String preTag;
+		@Override
+		public void startDocument() throws SAXException {
+			;
+		}
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes) 
+				throws SAXException {
+			if("ConneterBean".equals(qName)){
+				bean = new ConnecterBean();
+			}
+			preTag = qName;
+		}
+		@Override
+		public void characters(char[] ch, int start, int length)
+				throws SAXException {
+			if(bean != null){
+				if("driver".equals(preTag)) bean.setDriver(new String(ch,start,length));
+				else if("database".equals(preTag)) bean.setDatabase(new String(ch,start,length));
+				else if("username".equals(preTag)) bean.setUsername(new String(ch,start,length));
+				else if("password".equals(preTag)) bean.setPassword(new String(ch,start,length));
+			}
+		}
+		@Override
+		public void endElement(String uri, String localName, String qName)throws SAXException {
+			preTag = null;
+		}	
+		public ConnecterBean getConnecterBean(){
+			return bean;
+		}		
+}
+
+class ConnecterBean {
+	private String driver;
+	private String username;
+	private String password;
+	private String database;
+	public ConnecterBean(){
+		this.driver = "";
+		this.username = "";
+		this.password = "";
+		this.database = "";
+	}
+	public String getDriver() {
+		return driver;
+	}
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDatabase() {
+		return database;
+	}
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+	
+}
---
+++ FacilityBookingSystem/src/dto/Booking.java
@@ -0,0 +1,185 @@
+package data.dto;
+
+import java.io.Serializable;
+
+public class Booking implements Cloneable, Serializable {
+	private String bookingID;
+    private String userID;
+    private String facilityID;
+    private java.sql.Date starttime;
+    private Integer duration;
+    private String status;
+    private String priority;
+    private String reason;
+
+    public Booking () {
+
+    }
+
+    public Booking (String bookingIDIn) {
+          this.bookingID = bookingIDIn;
+    }
+
+    public String getBookingID() {
+          return this.bookingID;
+    }
+    public void setBookingID(String bookingIDIn) {
+          this.bookingID = bookingIDIn;
+    }
+
+    public String getUserID() {
+          return this.userID;
+    }
+    public void setUserID(String userIDIn) {
+          this.userID = userIDIn;
+    }
+
+    public String getFacilityID() {
+          return this.facilityID;
+    }
+    public void setFacilityID(String facilityIDIn) {
+          this.facilityID = facilityIDIn;
+    }
+
+    public java.sql.Date getStarttime() {
+          return this.starttime;
+    }
+    public void setStarttime(java.sql.Date starttimeIn) {
+          this.starttime = starttimeIn;
+    }
+
+    public Integer getDuration() {
+          return this.duration;
+    }
+    public void setDuration(Integer durationIn) {
+          this.duration = durationIn;
+    }
+
+    public String getStatus() {
+          return this.status;
+    }
+    public void setStatus(String statusIn) {
+          this.status = statusIn;
+    }
+
+    public String getPriority() {
+          return this.priority;
+    }
+    public void setPriority(String priorityIn) {
+          this.priority = priorityIn;
+    }
+
+    public String getReason() {
+          return this.reason;
+    }
+    public void setReason(String reasonIn) {
+          this.reason = reasonIn;
+    }
+
+    public void setAll(String bookingIDIn,
+          String userIDIn,
+          String facilityIDIn,
+          java.sql.Date starttimeIn,
+          Integer durationIn,
+          String statusIn,
+          String priorityIn,
+          String reasonIn) {
+          this.bookingID = bookingIDIn;
+          this.userID = userIDIn;
+          this.facilityID = facilityIDIn;
+          this.starttime = starttimeIn;
+          this.duration = durationIn;
+          this.status = statusIn;
+          this.priority = priorityIn;
+          this.reason = reasonIn;
+    }
+
+    public boolean hasEqualMapping(Booking valueObject) {
+
+          if (this.bookingID == null) {
+                    if (valueObject.getBookingID() != null)
+                           return(false);
+          } else if (!this.bookingID.equals(valueObject.getBookingID())) {
+                    return(false);
+          }
+          if (this.userID == null) {
+                    if (valueObject.getUserID() != null)
+                           return(false);
+          } else if (!this.userID.equals(valueObject.getUserID())) {
+                    return(false);
+          }
+          if (this.facilityID == null) {
+                    if (valueObject.getFacilityID() != null)
+                           return(false);
+          } else if (!this.facilityID.equals(valueObject.getFacilityID())) {
+                    return(false);
+          }
+          if (this.starttime == null) {
+                    if (valueObject.getStarttime() != null)
+                           return(false);
+          } else if (!this.starttime.equals(valueObject.getStarttime())) {
+                    return(false);
+          }
+          if (this.duration == null) {
+                    if (valueObject.getDuration() != null)
+                           return(false);
+          } else if (!this.duration.equals(valueObject.getDuration())) {
+                    return(false);
+          }
+          if (this.status == null) {
+                    if (valueObject.getStatus() != null)
+                           return(false);
+          } else if (!this.status.equals(valueObject.getStatus())) {
+                    return(false);
+          }
+          if (this.priority == null) {
+                    if (valueObject.getPriority() != null)
+                           return(false);
+          } else if (!this.priority.equals(valueObject.getPriority())) {
+                    return(false);
+          }
+          if (this.reason == null) {
+                    if (valueObject.getReason() != null)
+                           return(false);
+          } else if (!this.reason.equals(valueObject.getReason())) {
+                    return(false);
+          }
+
+          return true;
+    }
+
+    public String toString() {
+        StringBuffer out = new StringBuffer();
+        out.append("[ID= " + this.bookingID+"|"); 
+        out.append("UserID= " + this.userID+"|"); 
+        out.append("FacilityID= " + this.facilityID+"|"); 
+        out.append("Starttime= " + this.starttime+"|"); 
+        out.append("Duration= " + this.duration+"|"); 
+        out.append("Status= " + this.status+"|"); 
+        out.append("Priority= " + this.priority+"|"); 
+        out.append("Reason= " + this.reason+"|"); 
+        return out.toString();
+    }
+
+    public Object clone() {
+        Booking cloned = new Booking();
+
+        if (this.bookingID != null)
+             cloned.setBookingID(new String(this.bookingID)); 
+        if (this.userID != null)
+             cloned.setUserID(new String(this.userID)); 
+        if (this.facilityID != null)
+             cloned.setFacilityID(new String(this.facilityID)); 
+        if (this.starttime != null)
+             cloned.setStarttime((java.sql.Date)this.starttime.clone()); 
+        if (this.duration != null)
+             cloned.setDuration(new Integer(this.duration.intValue())); 
+        if (this.status != null)
+             cloned.setStatus(new String(this.status)); 
+        if (this.priority != null)
+             cloned.setPriority(new String(this.priority)); 
+        if (this.reason != null)
+             cloned.setReason(new String(this.reason)); 
+        return cloned;
+    }
+}
