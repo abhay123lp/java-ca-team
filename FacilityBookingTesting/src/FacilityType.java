72b18407df7d9669f472836563af8eff5e201e59
sue22889@gmail.com <sue22889@gmail.com@88132001-a776-28e8-4add-e9d731fe8be2>
2012-12-06 04:00:32 +0000

    just testing.... sori

---
+++ FacilityBookingTesting/src/FacilityType.java
@@ -0,0 +1,34 @@
+
+public class FacilityType {
+
+	private int typeID; 
+	private String typeName;
+	private int capacity;
+	
+	public int getTypeID() {
+		return typeID;
+	}
+	public void setTypeID(int typeID) {
+		this.typeID = typeID;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String description;
+	
+}
